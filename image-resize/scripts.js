b793e44d1394a251ae255afff5838c78cbd67669
turboflanagan <macbookpro@MacBook-Pros-MacBook-Pro.local>
2015-10-30 22:20:50 -0400

    Initial commit
    working on JS to dynamically load pics of different sizes into same
    size divs keeping aspect ratio and centering them within the div.

---
+++ image-resize/scripts.js
@@ -0,0 +1,11 @@
+
+// var imageInfo = $('.home-image-wrapper');
+var imageDiv = $("div").find(".home-image-wrapper");
+// console.log(imageDiv);
+$(window).load(function(){
+	for(i=0; i < imageDiv.length; i++) {
+		
+	} 
+
+
+})
---
+++ scripts.js
@@ -0,0 +1,27 @@
+
+// var imageInfo = $('.home-image-wrapper');
+var imageDiv = $("div").find(".home-image-wrapper");
+// console.log(imageDiv);
+$(window).load(function(){
+	for(i=0; i < imageDiv.length; i++) {
+
+	} 
+
+
+})
+
+//  psydo code for putting this project together.
+// wait for window to load
+//     get all elements that hold information
+//     took that list of elements grabbed the images inside of them
+//     loop through all of the images in this array{
+//         get image height
+//         get image width
+        
+//         check if height is greater than width
+//         if true {
+//             //scale everything based on height
+//             set image height to max height
+//             check new image width
+//             make sure image is centered by adding margin that is half the difference of the image width to the div width
+// }
